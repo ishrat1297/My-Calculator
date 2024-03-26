1a7c5498923cbe612d218b85869fccc89c1a6331
Your Name <you@example.com>
2024-03-26 17:40:54 +0500

    tweak the text and added console at the end

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "enter first number", type: "number", name: "firstnumber" },
-    { message: "enter second number", type: "number", name: "secondnumber" },
+    { message: "enter your first number", type: "number", name: "firstnumber" },
+    { message: "enter your second number", type: "number", name: "secondnumber" },
     {
         message: "select one of the operators to perform operation",
         type: "list",
@@ -25,3 +25,4 @@ else if (answer.operator === "division") {
 else {
     console.log("please select valid operator");
 }
+console.log("THE END");
